36a61596b70327a2b2c502cd61e7dacc356862f5
Gowthamraj Srinivasan <gowthamraj.srinivasan@cmgl.ca>
2025-08-19 07:21:08 +0530

    added: Google analytics to the website - uses same measurement ID as the chrome extension

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import ExtensionMonitorProvider from "@/components/ExtensionMonitorProvider";
+import Script from "next/script";
 
 const geistSans = Geist({ variable: "--font-geist-sans", subsets: ["latin"] });
 const geistMono = Geist_Mono({ variable: "--font-geist-mono", subsets: ["latin"] });
@@ -17,6 +18,17 @@ export default function RootLayout({
   return (
     <html lang="en" className="h-full">
       <body className={`${geistSans.variable} ${geistMono.variable} antialiased h-full`}>
+        {/* Google Analytics */}
+        <Script src="https://www.googletagmanager.com/gtag/js?id=G-7TVDEPTJ6R" strategy="afterInteractive" />
+        <Script id="google-analytics" strategy="afterInteractive">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-7TVDEPTJ6R');
+          `}
+        </Script>
+        
         <ExtensionMonitorProvider>
           {children}
         </ExtensionMonitorProvider>
